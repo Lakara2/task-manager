9549da08868ee89d962a14423b795eb50766e3d5
Lakara2 <lbryanlakara@gmail.com>
2023-06-15 11:23:35 +0300

    chore: add hours

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,73 @@
-import Head from "next/head";
 import { useRouter } from "next/router";
+import React from 'react';
 
-/**
-  Calculates the time difference between the server time and client time.
-  @param {Date} serverTime - The server time.
-  @param {Date} clientTime - The client time.
-  @returns {string} The time difference in the format "{days} days, {hours} hours, {minutes} minutes, {seconds} seconds".
-*/
-const calculateTimeDifference = (server: Date, client: Date) => {};
+interface HomeProps {
+  serverTime: string;
+}
+
+const Home: React.FC<HomeProps> = ({ serverTime }) => {
+  // Fonction pour calculer le temps écoulé en jours, heures, minutes et secondes
+  const formatElapsedTime = (dateString: string) => {
+    const currentTime = new Date();
+    const serverTime = new Date(dateString);
+    const elapsedTime = currentTime.getTime() - serverTime.getTime();
+
+    const seconds = Math.floor(elapsedTime / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+
+    return `${days} days, ${hours % 24} hours, ${minutes % 60} minutes, ${seconds % 60} seconds`;
+  };
+  
+  // Fonction pour formater la date au format "DD-MM-AAAA HH:mm"
+  const formatDate = (dateString: string) => {
+    const options: Intl.DateTimeFormatOptions = {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+    };
+    return new Date(dateString).toLocaleString('fr-FR', options);
+  };
+
+  const formattedElapsedTime = formatElapsedTime(serverTime);
+  const formattedServerTime = formatDate(serverTime);
 
 
-export default function Home() {
   const router = useRouter();
   const moveToTaskManager = () => {
     router.push("/tasks");
-  }
+  };
+  
+  const calculateTimeDifference = (serverTime: Date, clientTime: Date): string => {
+    const timeDifference = Math.floor((clientTime.getTime() - serverTime.getTime()) / 1000);
+  
+    const days = Math.floor(timeDifference / (24 * 60 * 60));
+    const hours = Math.floor((timeDifference % (24 * 60 * 60)) / (60 * 60));
+    const minutes = Math.floor((timeDifference % (60 * 60)) / 60);
+    const seconds = Math.floor(timeDifference % 60);
+  
+    return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+  };
+  
+
   return (
     <>
-      <Head>
-        <title>Web 2 - Exam TD</title>
-        <meta name="description" content="Just an exam" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <main>
         <h1>The easiest exam you will ever find</h1>
         <div>
-          {/* Display here the server time (DD-MM-AAAA HH:mm)*/}
           <p>
             Server time:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            <span className="serverTime">
+              {formattedServerTime}
+            </span>
           </p>
 
-          {/* Display here the time difference between the server side and the client side */}
           <p>
             Time diff:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            <span className="serverTime">{formattedElapsedTime}</span>
           </p>
         </div>
 
@@ -46,3 +78,15 @@ export default function Home() {
     </>
   );
 }
+
+export async function getServerSideProps() {
+  const serverTime = new Date().toString(); // Récupère l'heure actuelle du serveur
+
+  return {
+    props: {
+      serverTime,
+    },
+  };
+}
+
+export default Home;
